71fe628836ea81c1572e83b22e6a01f0cd62330b
mgembree <mgembree@ucsc.edu>
2024-10-21 16:51:55 -0700

    Lab 5 Assignment changes
    Completed lab 5 by adding JS file and outputting js & image.

--- lab5/js/lab.js
+++ lab5/js/lab.js
@@ -1,20 +1,25 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// index.js - Lab 5: Variable 
+// Author: Matthew Embree
+// Date: 10/21/2024
 
 // Constants
 
-// Functions
-
-// this is an example function and this comment tells what it doees and what parameters are passed to it.
-function myFunction(param1, param2) {
-  // some code here
-  // return results;
-}
-
 function main() {
-  console.log("Main function started.");
-  // the code that makes everything happen
+//Define Variables
+make = "Mercedez-Benz";
+model = "300 SD";
+color = "grey";
+year = 1980;
+//age calculation
+age = 2024 - year;
+
+//output
+document.writeln("Make: " + make +"<br>");
+document.writeln("Model: " + model +"<br>");
+document.writeln("Color: " + color  +"<br>");
+document.writeln("Year: " + year +"<br>");
+document.writeln("Age: " + age  +" years<br>");
+
 }
 
 // let's get this party started
