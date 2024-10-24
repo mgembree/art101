471c20840672d140892a8bced64da8d433d3d407
mgembree <mgembree@ucsc.edu>
2024-10-24 15:33:08 -0700

    Lab 6 - Arrays and Objects
    Completed Lab 6, used chatgpt to organize my html and figure out how to output the JS properly.

--- lab6/js/lab.js
+++ lab6/js/lab.js
@@ -1,21 +1,23 @@
 // index.js - purpose and description here
-// Author: Your Name
-// Date:
+// Author: Matthew Embree
+// Date: 10/24/2024
+
+var myMainRide = {
+  make: "Mercedez-Benz",
+  model: "300-SD",
+  year: 1980,
+  age: 2024 - 1980, // Fixed age calculation
+  color: "grey",
+};
 
 // Constants
-
-// Functions
-
-// this is an example function and this comment tells what it doees and what parameters are passed to it.
-function myFunction(param1, param2) {
-  // some code here
-  // return results;
-}
+const myTransport = ["Bus", "Walking", "Skateboard"];
 
 function main() {
-  console.log("Main function started.");
-  // the code that makes everything happen
+  // Output to .output div instead of document.writeln()
+  document.querySelector('.output').innerHTML = 
+    "My modes of transport: " + myTransport.join(", ") + "<br>" + 
+    "My Main Ride: <pre>" + JSON.stringify(myMainRide, null, 2) + "</pre>";
 }
 
-// let's get this party started
 main();
