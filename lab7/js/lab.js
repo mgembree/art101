8b1802eaf45625041463c88e35d405df5a5a695a
mgembree <mgembree@ucsc.edu>
2024-10-28 17:30:33 -0700

    Lab 7: Functions
    Completed Lab 7: Functions
    Updated album of week and added song.

---
+++ lab7/js/lab.js
@@ -0,0 +1,11 @@
+// index.js - purpose and description here
+// Author: Matthew Embree
+// Date: 10/28/2024
+
+//Function doSomething takes a user input, their name, and sorts the characters
+//of the name and rearranges them in a string
+function doSomething() {
+  let userName = window.prompt("Enter your username: ");     //User input
+  return sortedUserName = userName.split('').sort().join();     //1 Split string into array/ 2 sort characters in array / 3 rejoin sorted char-array into string
+}
+document.querySelector('.output').innerHTML = "Sorted Name: " + doSomething();      //Output the sorted name 
