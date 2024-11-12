d82d3152f9b2296304c9b28c3a7dcf5a01a80d21
mgembree <mgembree@ucsc.edu>
2024-11-11 18:47:17 -0800

    Lab 11 completion
    Completed lab 11 added link to homepage.

---
+++ lab11/js/lab.js
@@ -0,0 +1,16 @@
+// index.js - Lab 11: Javascript Events and Forms
+// Author: Matthew Embree
+// Date: 11/11/2024
+
+
+//This function returns a string after being split, sorted, and joined back together as a string
+function sortString(inputString) {
+  return inputString.split('').sort().join(''); }
+//Event listener for clicking button ID: submit
+$("#submit").click(function(){
+  const userName = $("#user-name").val();   //ID: user-name from html form
+  userNameSorted = sortString(userName);    //sorts user-name from html form.
+  $("#output").html.append('<div class="text"><p>' + userNameSorted + '</p></div>');     //appends a new div to output containing the user name after sorting
+});
+
+
